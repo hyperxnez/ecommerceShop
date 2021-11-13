c7f6f48d90ae47b138c44da87a2986962fd8f3a0
hyperxnez <a16.gv.vl@gmail.com>
2021-11-13 23:11:56 +0700

    update 13/11

---
+++ ASP_NETCore_Working_EF/ASP_NETCore_Working_EF/Controllers/BillCustomer.cs
@@ -0,0 +1,17 @@
+﻿using ASP_NETCore_Working_EF.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NETCore_Working_EF.Controllers
+{
+    public class BillCustomer : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
